fd7613cf5f8603f22f0c42fa0181d7f140eca18b
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-04-03 20:24:00 -0300

    feat: refact Main

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,16 +1,10 @@
 package br.com.fiap;
 
-import br.com.fiap.dao.impl.BuyerDAOImpl;
-import br.com.fiap.dao.impl.CartDAOImpl;
-import br.com.fiap.entity.Buyer;
-import br.com.fiap.entity.Cart;
 import br.com.fiap.exception.CommitException;
 import br.com.fiap.singleton.EntityManagerFactorySingleton;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import java.sql.Date;
-import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) throws CommitException {
