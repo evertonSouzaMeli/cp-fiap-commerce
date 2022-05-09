823e0f150d00f172273ed4ada6fb1aee8d1a61a5
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-05-08 23:39:06 -0300

    minor changes

--- query_example.sql
+++ query_example.sql
@@ -2,4 +2,4 @@ SELECT * FROM TB_BUYER buyer JOIN TB_ADDRESS address ON buyer.ADDRESS_ID = addre
 
 SELECT * FROM TB_STOCK stock JOIN TB_PRODUCT product ON stock.ID = product.STOCK_ID;
 
-SELECT * FROM TB_CART cart JOIN TB_CART_PRODUCT cart_product ON cart.ID = CARTLIST_ID JOIN TB_PRODUCT product ON product.ID = cart_product.PRODUCTLIST_ID;
+SELECT * FROM TB_CART cart JOIN TB_CART_PRODUCT cart_product ON cart.ID = cart_product.CART_ID JOIN TB_PRODUCT product ON product.ID = cart_product.PRODUCT_ID;
